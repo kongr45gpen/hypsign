7b24d083d3b194212060e64104e5dddf6f6ace5d
kongr45gpen <electrovesta@gmail.com>
2023-04-25 23:29:20 +0200

    Automatically update display list

--- signage/consumers.py
+++ signage/consumers.py
@@ -8,11 +8,12 @@ from django.http import Http404
 from djangochannelsrestframework.decorators import action
 from djangochannelsrestframework.observer import observer
 from djangochannelsrestframework.consumers import AsyncAPIConsumer
+from asgiref.sync import SyncToAsync
 
 from signage.models import Display
 from rest_framework.exceptions import NotFound
 from signage.serializers import PageSerializer
-from .signals import display_update_signal
+from .signals import display_update_signal, display_connect_signal
 
 from channels import layers
 from rest_framework import status
@@ -20,17 +21,22 @@ from rest_framework import status
 logger = logging.getLogger("websockets")
 
 lock = asyncio.Lock()
-subscribed_displays = {}
+connected_displays = {}
 
 class SignageConsumer(AsyncAPIConsumer):
+    # Code of the connected display, if any
+    code = None
+
     async def disconnect(self, close_code):
         async with lock:
-            if self.channel_name in subscribed_displays:
-                del subscribed_displays[self.channel_name]
+            if self.channel_name in connected_displays:
+                del connected_displays[self.channel_name]
+                await SyncToAsync(display_connect_signal.send)(sender=self.__class__, data={'code': self.code})
 
-    async def add_to_subscribed_displays(self, data):
+    async def _add_to_connected_displays(self, data):
         async with lock:
-            subscribed_displays[self.channel_name] = {**data, 'ip_address': self.scope['client'][0]}
+            connected_displays[self.channel_name] = {**data, 'ip_address': self.scope['client'][0]}
+        await SyncToAsync(display_connect_signal.send)(sender=self.__class__, data=data)
 
     @action()
     async def hello(self, code, **kwargs):
@@ -40,8 +46,10 @@ class SignageConsumer(AsyncAPIConsumer):
             logger.warning("Display not found: {}".format(code))
             raise NotFound("Display not found")
 
+        self.code = code
+
         await self.display_updated_handler.subscribe(code=code, **kwargs)
-        asyncio.create_task(self.add_to_subscribed_displays({'code': code, 'display': kwargs['display']}))
+        asyncio.create_task(self._add_to_connected_displays({'code': code, 'display': kwargs['display']}))
 
         logger.debug("Received hello from display: {}".format(display.code))
         return {}, status.HTTP_204_NO_CONTENT
@@ -63,10 +71,10 @@ class SignageConsumer(AsyncAPIConsumer):
         return PageSerializer(page).data, 200
     
     @action()
-    async def get_subscribed_displays(self, **kwargs):
+    async def get_connected_displays(self, **kwargs):
         async with lock:
-            logger.debug("Found {} subscribed displays".format(len(subscribed_displays)))
-            return list(subscribed_displays.values()), 200
+            logger.debug("Found {} subscribed displays".format(len(connected_displays)))
+            return list(connected_displays.values()), 200
     
     @observer(signal=display_update_signal)
     async def display_updated_handler(self, data, observer=None, action=None, subscribing_request_ids=[], **kwargs):
@@ -83,3 +91,13 @@ class SignageConsumer(AsyncAPIConsumer):
     def display_updated_handler(self, code, **kwargs):
         if code:
             yield f'display__{code}'
+
+    @action()
+    async def subscribe_to_display_connections(self, **kwargs):
+        await self.display_connected_handler.subscribe(**kwargs)
+        return {}, status.HTTP_204_NO_CONTENT 
+
+    @observer(signal=display_connect_signal)
+    async def display_connected_handler(self, data, observer=None, action=None, subscribing_request_ids=[], **kwargs):
+        for request_id in subscribing_request_ids:
+            await self.handle_action(action='get_connected_displays', request_id=request_id)
--- signage/signals.py
+++ signage/signals.py
@@ -1,4 +1,8 @@
 # signals.py
 from django.dispatch.dispatcher import Signal
 
+# Signal sent whenever the content of a display is updated
 display_update_signal = Signal()
+
+# Signal sent whenever a display is connected or disconnected
+display_connect_signal = Signal()
