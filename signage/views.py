779382de1d50ba2a45108751f017b769b64c7ca6
kongr45gpen <electrovesta@gmail.com>
2023-04-28 00:57:23 +0200

    Improve display diagnostics

--- signage/views.py
+++ signage/views.py
@@ -7,8 +7,14 @@ def diagnosis_view(request, display_code):
     display = get_object_or_404(Display, code=display_code)
 
     schedule_item = display.get_current_schedule_item()
+    page = None
+
+    if schedule_item:
+        sequence_item = schedule_item.get_current_sequence_item()
+        if sequence_item:
+            page = sequence_item.page
     
-    return render(request, 'signage/debug.html', {'display': display, 'schedule': schedule_item})
+    return render(request, 'signage/debug.html', {'display': display, 'schedule': schedule_item, 'page': page})
 
 def display_view(request, display_code):
     display = get_object_or_404(Display, code=display_code)
