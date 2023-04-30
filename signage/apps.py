5a3598a75e8690a9d14cdf1fab910a5cfc52e551
kongr45gpen <electrovesta@gmail.com>
2023-04-30 03:34:15 +0200

    Fix first-time install

--- signage/apps.py
+++ signage/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
 
+def is_database_synchronized(database):
+    from django.db.migrations.executor import MigrationExecutor
+    from django.db import connections
+    connection = connections[database]
+    connection.prepare_database()
+    executor = MigrationExecutor(connection)
+    targets = executor.loader.graph.leaf_nodes()
+    return not executor.migration_plan(targets)
 
 class SignageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -7,15 +15,17 @@ class SignageConfig(AppConfig):
 
     def ready(self):
         from django_q.models import Schedule
+        from django.db import DEFAULT_DB_ALIAS
 
-        Schedule.objects.update_or_create(name='keepalive', defaults={
-            'func': 'signage.tasks.send_keepalive',
-            'schedule_type': Schedule.MINUTES,
-            'minutes': 1,
-        }) 
+        if is_database_synchronized(DEFAULT_DB_ALIAS):
+            Schedule.objects.update_or_create(name='keepalive', defaults={
+                'func': 'signage.tasks.send_keepalive',
+                'schedule_type': Schedule.MINUTES,
+                'minutes': 1,
+            }) 
 
-        Schedule.objects.update_or_create(name='checkupdates', defaults={
-            'func': 'signage.tasks.check_for_updates',
-            'schedule_type': Schedule.MINUTES,
-            'minutes': 1,
-        }) 
+            Schedule.objects.update_or_create(name='checkupdates', defaults={
+                'func': 'signage.tasks.check_for_updates',
+                'schedule_type': Schedule.MINUTES,
+                'minutes': 1,
+            }) 
